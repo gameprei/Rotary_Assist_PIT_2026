725c7a4e507b8c5b7c16491019b14054f9020a88
gameprei <luizhenrique9474@gmail.com>
2026-03-09 23:58:36 -0300

    Adição das rotas cadastrar, editar, excluir fornecedores

--- rotary-backend/src/routes/FornecedorRoutes.js
+++ rotary-backend/src/routes/FornecedorRoutes.js
@@ -7,5 +7,11 @@ const router = express.Router();
 router.get("/fornecedores", FornecedorController.listarTodos);
 // Rota para buscar fornecedores por termo (nome || cnpj)
 router.get("/fornecedores/:termo", FornecedorController.buscarPorTermo);
+// Rota para cadastrar novo fornecedor
+router.post("/fornecedores", FornecedorController.cadastrar);
+// Rota para atualizar fornecedor 
+router.put("/fornecedores/:id", FornecedorController.atualizar);
+// Rota para excluir fornecedor
+router.delete("/fornecedores/:id", FornecedorController.excluir);
 
 export default router;
